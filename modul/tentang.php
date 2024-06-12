62d5986fd2417fa6aded9a238ca402714b8b0b1c
Aji Setiawan <ajiksetiawan21@gmail.com>
2024-06-12 22:03:44 +0700

    first commit

---
+++ modul/tentang.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+   <title>Tentang</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='aset/tentang/style.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+   <link rel="stylesheet" href="aset/login/css/style.css">
+  
+  
+</head>
+<style>
+@media only screen and (max-width: 400px) {
+    img {
+        width: 100%;
+    }
+}
+</style>
+<body>
+
+  <div class="ma3">
+  <article class="tc w-75 center pt5 pb2 ph3 mw6-ns ba bw1 b--light-gray" style="background: #fff;">
+    <header class="mb3">
+		<img src="favicon.ico" alt="Profile headshot" />
+      <h1 class="f3 lh-title mv2 dark-gray">Sistem Pakar</h1>
+	  <p class=" silver mt2 mb1"><a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Pengembang Aplikasi"><i class="fa fa-user" aria-hidden="true"></i> Aji Setiawan</button></a>
+<a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Pakar Aplikasi"><i class="fa fa-user-md" aria-hidden="true"></i> Drh. Eni Widayati</button></a>
+	 <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Dosen Pembimbing 1"><i class="fa fa-user-md" aria-hidden="true"></i> Dwi Hartanti,S.Kom.,M.Kom</button></a></p>
+	 <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Dosen Pembimbing 2"><i class="fa fa-user-md" aria-hidden="true"></i> MOH. Muhtarom, SE.,S.Kom.,M.Kom</button></a></p>  
+   <p  class="f6 tl lh-copy silver" style=" margin: 20px; ">Sistem pakar yang mampu mendiagnosa penyakit pada ayam berdasarkan pengetahuan yang diberikan langsung dari pakar/ahlinya dan melalui studi  literatur. Penelitian ini menggunakan metode perhitungan Certainty Factor (CF) dalam menghitung tingkat kepakaran. Data penelitian ini terdiri dari data gejala dan data penyakit ayam, serta data aturan. Sistem pakar pada organisasi ditujukan  untuk penambahan  value, peningkatan produktivitas serta area manajerial yang dapat mengambil kesimpulan dengan cepat.</p>
+	   <h2 class="f5 silver mt2 mb1">Sistem Pakar, Diagnosis Penyakit Pada Ayam</h2>
+	  <h2 class="f5 silver mt2 mb1">Copyright © 2023, <a class="link dim silver">Universitas Duta Bangsa Surakarta</a></h2>
+	  <br>
+      <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-chrome" aria-hidden="true"></i></a>
+      <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-firefox" aria-hidden="true"></i></a>
+      <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-edge" aria-hidden="true"></i></a>
+	  <a class="link dim light-silver"  style="font-size: 40px;"><i class="fa fa-safari" aria-hidden="true"></i></a>
+	  <a class="link dim light-silver"  style="font-size: 40px;"> | </a>
+	       <a class="link dim light-silver"  style="font-size: 40px;"> 	<img src="aset/tentang/logo2.png" alt="Profile headshot" /></a>
+        
+        </header>
+  </article>
+</div>
+</body>
+
+</html>
